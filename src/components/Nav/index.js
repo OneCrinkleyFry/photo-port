aae434e48369a2623a46da2d2d9d6abb47a91405
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-15 11:40:29 -0700

    added a usestate to the app.js

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-
-  const  categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
-  ];
-
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
   const handleClick = (item) => {
     console.log(item);
     return item;
@@ -36,9 +33,9 @@ function Nav() {
           </li>
           {
             categories.map((category) => (
-              <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(category.name); }}>
-                 {capitalizeFirstLetter(category.name)}
+              <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name} >
+                <span onClick={() => { setCurrentCategory(category) }}>
+                  {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))
