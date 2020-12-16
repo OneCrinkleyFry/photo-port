45d5cf9882bb124b4da8eee0aea99eb1943d0439
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-16 10:19:22 -0700

    added the form submission features

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,21 +6,30 @@ function ContactForm() {
 
    const { name, email, message } = formState;
 
+   function handleChange(e) {
+      setFormState({...formState, [e.target.name]: e.target.value });
+   }
+
+   function handleSubmit(e) {
+      e.preventDefault();
+      console.log(formState);
+   }
+
    return (
       <section>
          <h1>Contact Me</h1>
-         <form id='contact-form'>
+         <form id='contact-form' onSubmit={handleSubmit}>
             <div>
                <label htmlFor='name' style={{visibility: 'hidden', display: 'none'}}>Name:</label>
-               <input type='text' name='name' placeholder='Name' defaultValue={name} />
+               <input type='text' name='name' placeholder='Name' defaultValue={name} onChange={handleChange} />
             </div>
             <div>
                <label htmlFor='email' style={{visibility: 'hidden', display: 'none'}}>Email:</label>
-               <input type='email' name='email' placeholder='Email' defaultValue={email}/>
+               <input type='email' name='email' placeholder='Email' defaultValue={email} onChange={handleChange} />
             </div>
             <div>
                <label htmlFor='message' style={{visibility: 'hidden', display: 'none'}}>Message:</label>
-               <textarea name='message' placeholder='Message:' defaultValue={message}/>
+               <textarea name='message' placeholder='Message:' defaultValue={message} onChange={handleChange} />
             </div>
             <button type='submit'>Submit</button>
          </form>
