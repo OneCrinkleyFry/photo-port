65c470791b9ee856b5b53b7fdd7d9a824e807392
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-12-17 11:08:38 -0700

    added the modal content

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function Modal() {
-
+function Modal({currentPhoto, setIsModalOpen }) {
+   const {name, category, description, index} = currentPhoto;
+   
    return (
       <div className='modalBackdrop'>
          <div className='modalContainer'>
-            <h3 className='modalTitle'>Photo Name</h3>
-            <img alt='current category' />
-            <p>Photo Description</p>
-            <button type='button'>Close This Modal</button>
+            <h3 className='modalTitle'>{name}</h3>
+            <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt={category} />
+            <p>{description}</p>
+            <button type='button' onClick={()=> setIsModalOpen(false)}>Close This Modal</button>
          </div>
       </div>
    );
